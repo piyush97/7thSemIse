55213975bff97e6936467323dfc9d779682feffb
piyush97 <me@piyushmehta.com>
2019-12-25 22:43:51 +0530

    gbu

---
+++ finals-DM/PartA/A9/A9.r
@@ -0,0 +1,8 @@
+install.packages("factoextra")
+library(factoextra)
+library(cluster)
+data<-read.csv("/Users/piyushmehta/7thSemIse/finals-DM/PartA/A9/movie.csv")
+d<- scale(dist(data,method="euclidean"))
+kfit<- kmeans(d,3)
+fviz_cluster(kfit,data)
+plot(kfit)
