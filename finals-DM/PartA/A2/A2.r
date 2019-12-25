9d00202c4b4a5996e5a7736b8dc23b512ee8cc30
piyush97 <me@piyushmehta.com>
2019-12-25 22:44:08 +0530

    gbu

--- finals-DM/PartA/A2/A2.r
+++ finals-DM/PartA/A2/A2.r
@@ -1,31 +1,16 @@
-# Install packages use only 1 time
-install.packages("party")
-install.packages("e1071")
-install.packages("caret")
-
-#load libraries
-
-library(party)  #Contains the decision tree functions
-library(caret)  #Contains confusion matrix functions
-library(e1071)  #Contains the naive bayes functions
-
-data <- read.csv("/Users/piyushmehta/7thSemIse/finals-DM/fever.csv")
-View(data)
-
-#Split dataset into test and train
-index <- sample(2, nrow(data), replace=TRUE, prob=c(0.7,0.3))
-train <- data[index==1,]
-test <- data[index==2,]
-
-#Select the dependent and independent features
-features <- Patient_Name ~ Age + Fever
-
-
-#DECISION TREE
-
-#Obtain a decision tree model
-model <- ctree(features, data=train)
-
-#Plot the model
-plot(model)
-
+library(rpart)
+library(rpart.plot)
+library(party)
+library(caret)
+data<- read.csv("/Users/piyushmehta/7thSemIse/finals-DM/PartA/A2/fever.csv")
+data
+index<- sample(2, nrow(data), TRUE, c(0.7,0.3))
+train<- data[index==1,]
+test<- data[index==2,]
+features<- Result~i1+i2+i3+i4
+tree<- rpart(features, train, method="class")
+rpart.plot(tree)
+tree2<- ctree(features,train)
+plot(tree2)
+res<- predict(tree2,test)
+confusionMatrix(res1, test$Result)
--- finals-DM/PartA/A3/A3.r
+++ finals-DM/PartA/A3/A3.r
@@ -4,8 +4,8 @@ library(party)  #Contains the decision tree functions
 library(caret)  #Contains confusion matrix functions
 library(e1071)  #Contains the naive bayes functions
 
-data <- read.csv("/Users/piyushmehta/7thSemIse/finals-DM/fever.csv")
-View(data)
+data <- read.csv("/Users/piyushmehta/7thSemIse/finals-DM/A3/hopital.csv")
+data
 
 #Split dataset into test and train
 index <- sample(2, nrow(data), replace=TRUE, prob=c(0.7,0.3))
@@ -13,16 +13,16 @@ train <- data[index==1,]
 test <- data[index==2,]
 
 #Select the dependent and independent features
-features <- Patient_Name ~ Age + Fever
+features <- Type~i1+i2+i3+i4
 
 #NAIVE BAYES CLASSIFIER
 
 
-model2 <- naiveBayes(features, data=train)
+model <- naiveBayes(features, data=train)
 
 #Model Summary
-print(model2)
+print(model)
 
 #Evaluate model on test data
-test_predictions2 <- predict(model2, newdata = test)
-confusionMatrix(test_predictions2, test$fever, positive="yes")
+res <- predict(model, newdata = test)
+confusionMatrix(res, test$Type)
