090b57a5c22f8e62e14a71b839d56c3ef50f0592
piyush97 <me@piyushmehta.com>
2019-12-26 11:14:31 +0530

    Linear Regression working

---
+++ finals-DM/PartA/LinearRegression.r
@@ -0,0 +1,12 @@
+data<-read.csv("/Users/piyushmehta/7thSemIse/finals-DM/PartA/climate.csv")
+data
+hum<- data$humidity
+moist<- data$moisture
+features<- moist~hum
+new_data<- data.frame(hum, moist)
+model<- lm(features,new_data)
+plot(features)
+abline(model)
+test<- data.frame(num=c(200))
+ans<-prefict(model,test)
+ans
